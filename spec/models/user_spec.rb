02487f1819c3555927d60a9d7911784f3cb0b590
Dave Albert <dave.albert@gmail.com>
2013-01-03 06:31:42 +0000

    Listing 6.32

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,6 +44,7 @@ describe User do
 		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
 	end
+
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
@@ -51,6 +52,16 @@ describe User do
 				@user.email = invalid_address
 				@user.should_not be_valid
 			end
+		end
+	end
+
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
 		end
 	end
 

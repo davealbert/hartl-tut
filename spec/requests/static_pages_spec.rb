e2c9dd229777e1a09493e9c596eff9704b55b850
Dave Albert <dave.albert@gmail.com>
2013-01-03 05:08:49 +0000

    Listing 5.38

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,8 +36,26 @@ describe "Static pages" do
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact Us' }
+    let(:page_title) { 'Contact Us' }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "all static pages"
+  end
+
+
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact Us')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 end

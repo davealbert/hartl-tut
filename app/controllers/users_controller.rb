f376e33a116588fe2fdb0611402af878d044ea10
Dave Albert <dave.albert@gmail.com>
2013-01-05 13:52:10 +0000

    starting: 9.2.3 Friendly forwarding
    http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users#sec-friendly_forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
@@ -38,5 +39,10 @@ class UsersController < ApplicationController
   private
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end

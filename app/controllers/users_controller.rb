0a2f3009d3016b75db7c7cda54e5f28d6efb47c5
Dave Albert <dave.albert@gmail.com>
2013-01-05 14:03:06 +0000

    Starting: 9.3 Showing all users
    http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users#sec-showing_all_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    # @user = User.find(params[:id])
   end
 
   def update
@@ -38,7 +37,10 @@ class UsersController < ApplicationController
 
   private
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user

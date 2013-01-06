6949f94a96486f809434e34dba901765d6744ec0
Dave Albert <dave.albert@gmail.com>
2013-01-06 16:43:38 +0000

    WIP -- Listing 9.42. The attr_accessible attributes for the User model without an :admin attribute. app/models/user.rb
    http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users#code-attr_accessible_review

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-		# == Schema Information
+# == Schema Information
 #
 # Table name: users
 #
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 require 'spec_helper'
@@ -15,7 +16,7 @@ require 'spec_helper'
 describe User do
 
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com",	
+		@user = User.new(name: "Example User", email: "user@example.com",
 			password: "foobar", password_confirmation: "foobar")
 	end
 
@@ -27,9 +28,20 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
 	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
+	it { should_not be_admin }
+
+	describe "with admin attribute set to 'true'" do
+		before do
+			@user.save!
+			@user.toggle!(:admin)
+		end
+
+		it { should be_admin }
+	end
 
 	describe "remember token" do
 		before { @user.save }
